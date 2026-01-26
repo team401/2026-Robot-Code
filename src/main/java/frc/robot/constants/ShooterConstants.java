2b450ccebc06fe05ebe279b57e7431d590c5a2e8
aidnem <99768676+aidnem@users.noreply.github.com>
2026-01-26 13:59:51 -0500

    (#23) Implement shooter subsystem

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -1,8 +1,35 @@
 package frc.robot.constants;
 
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.KilogramSquareMeters;
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Second;
+
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import coppercore.parameter_tools.json.annotations.AfterJsonLoad;
 import coppercore.parameter_tools.json.annotations.JSONExclude;
+import coppercore.wpilib_interface.subsystems.configs.CANDeviceID;
+import coppercore.wpilib_interface.subsystems.configs.MechanismConfig;
+import coppercore.wpilib_interface.subsystems.configs.MechanismConfig.GravityFeedforwardType;
+import coppercore.wpilib_interface.subsystems.sim.CoppercoreSimAdapter;
+import coppercore.wpilib_interface.subsystems.sim.DCMotorSimAdapter;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.MomentOfInertia;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
 public class ShooterConstants {
   public final Double[] distanceToViDistancesMeters = {1.8, 2.0, 3.5};
@@ -38,5 +65,99 @@ public class ShooterConstants {
     }
 
     return distanceToVi.get(distanceMeters);
+  }
+
+  public final Integer shooterLeadMotorId = 14;
+  public final Integer shooterTopFollowerMotorId = 15;
+  public final Integer shooterBottomFollowerMotorId = 16;
+
+  public final Double shooterKP = 0.0;
+  public final Double shooterKI = 0.0;
+  public final Double shooterKD = 0.0;
+  public final Double shooterKS = 0.0;
+  public final Double shooterKV = 0.0;
+  public final Double shooterKA = 0.0;
+
+  /**
+   * The number of rotations the shooter motors rotate for each rotation of the output flywheels. If
+   * this value is less than one, it is an "upduction" (the flywheels will spin faster than the
+   * motors).
+   */
+  public final Double shooterReduction = 5.0 / 8.0;
+
+  /** Per-motor supply current limit */
+  public final Current shooterSupplyCurrentLimit = Amps.of(40.0);
+
+  /** Per-motor stator current limit */
+  public final Current shooterStatorCurrentLimit = Amps.of(40.0);
+
+  public final InvertedValue shooterMotorDirection =
+      InvertedValue.CounterClockwise_Positive; // TODO: Actual value
+  // TODO: Actual invert values for the followers
+  public final Boolean invertTopFollower = true;
+  public final Boolean invertBottomFollower = true;
+
+  // Perhaps regenerative braking can improve our battery performance?
+  public final NeutralModeValue defaultShooterNeutralMode = NeutralModeValue.Brake;
+
+  public final AngularVelocity shooterMaxVelocity = RPM.of(2900); // TODO: Real value
+  public final AngularAcceleration shooterMaxAcceleration = RPM.of(1000).per(Second);
+
+  public TalonFXConfiguration buildTalonFXConfigs() {
+    return new TalonFXConfiguration()
+        .withSlot0(
+            new Slot0Configs()
+                .withKP(shooterKP)
+                .withKI(shooterKI)
+                .withKD(shooterKD)
+                .withKS(shooterKS)
+                .withKG(0.0)
+                .withKV(shooterKV)
+                .withKA(shooterKA))
+        .withCurrentLimits(
+            new CurrentLimitsConfigs()
+                .withStatorCurrentLimit(shooterStatorCurrentLimit)
+                .withStatorCurrentLimitEnable(true)
+                .withSupplyCurrentLimit(shooterSupplyCurrentLimit)
+                .withSupplyCurrentLimitEnable(true))
+        .withMotorOutput(
+            new MotorOutputConfigs()
+                .withInverted(shooterMotorDirection)
+                .withNeutralMode(defaultShooterNeutralMode))
+        .withMotionMagic(
+            new MotionMagicConfigs()
+                .withMotionMagicAcceleration(shooterMaxAcceleration)
+                .withMotionMagicCruiseVelocity(shooterMaxVelocity))
+        .withFeedback(
+            new FeedbackConfigs()
+                .withFeedbackSensorSource(FeedbackSensorSourceValue.RotorSensor)
+                .withSensorToMechanismRatio(shooterReduction));
+  }
+
+  public MechanismConfig buildMechanismConfig() {
+    CANBus bus = new CANBus(JsonConstants.robotInfo.canivoreBusName);
+    return MechanismConfig.builder()
+        .withName("Shooter")
+        .withEncoderToMechanismRatio(shooterReduction)
+        .withMotorToEncoderRatio(1.0)
+        .withGravityFeedforwardType(GravityFeedforwardType.STATIC_ELEVATOR)
+        .withLeadMotorId(new CANDeviceID(bus, shooterLeadMotorId))
+        .addFollower(new CANDeviceID(bus, shooterTopFollowerMotorId), invertTopFollower)
+        .addFollower(new CANDeviceID(bus, shooterBottomFollowerMotorId), invertBottomFollower)
+        .build();
+  }
+
+  // TODO: Find a reasonable value for this MOI
+  public final MomentOfInertia shooterMOI = KilogramSquareMeters.of(0.01);
+
+  public CoppercoreSimAdapter buildShooterSim() {
+    return new DCMotorSimAdapter(
+        buildMechanismConfig(),
+        new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(
+                DCMotor.getKrakenX60Foc(3),
+                shooterMOI.in(KilogramSquareMeters),
+                1 / shooterReduction),
+            DCMotor.getKrakenX60Foc(3)));
   }
 }
---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -0,0 +1,209 @@
+package frc.robot.subsystems.shooter;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Second;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
+
+import coppercore.controls.state_machine.StateMachine;
+import coppercore.parameter_tools.LoggedTunableNumber;
+import coppercore.wpilib_interface.MonitoredSubsystem;
+import coppercore.wpilib_interface.subsystems.motors.MotorIO;
+import coppercore.wpilib_interface.subsystems.motors.MotorInputsAutoLogged;
+import coppercore.wpilib_interface.subsystems.motors.profile.MotionProfileConfig;
+import edu.wpi.first.units.measure.MutAngularVelocity;
+import frc.robot.DependencyOrderedExecutor;
+import frc.robot.DependencyOrderedExecutor.ActionKey;
+import frc.robot.constants.JsonConstants;
+import frc.robot.subsystems.shooter.ShooterState.TestModeState;
+import frc.robot.subsystems.shooter.ShooterState.VelocityControlState;
+import frc.robot.util.TestModeManager;
+import org.littletonrobotics.junction.Logger;
+
+/**
+ * A shooter powered by 3 kraken x60s running closed-loop velocity control with a Motion Magic
+ * Velocity profile.
+ */
+public class ShooterSubsystem extends MonitoredSubsystem {
+  public static final ActionKey UPDATE_INPUTS = new ActionKey("ShooterSubsystem::updateInputs");
+
+  private final TestModeManager<TestMode> testModeManager =
+      new TestModeManager<>("Shooter", TestMode.class);
+
+  // Motors and inputs
+  private final MotorInputsAutoLogged leadMotorInputs = new MotorInputsAutoLogged();
+  private final MotorInputsAutoLogged topFollowerMotorInputs = new MotorInputsAutoLogged();
+  private final MotorInputsAutoLogged bottomFollowerMotorInputs = new MotorInputsAutoLogged();
+
+  private final MotorIO leadMotor;
+  private final MotorIO topFollowerMotor;
+  private final MotorIO bottomFollowerMotor;
+
+  // State machine and states
+  private final StateMachine<ShooterSubsystem> stateMachine;
+
+  private final ShooterState velocityControlState;
+  private final ShooterState testModeState;
+
+  // Tunable Numbers
+  LoggedTunableNumber shooterKP;
+  LoggedTunableNumber shooterKI;
+  LoggedTunableNumber shooterKD;
+
+  LoggedTunableNumber shooterKS;
+  LoggedTunableNumber shooterKV;
+  LoggedTunableNumber shooterKA;
+
+  LoggedTunableNumber shooterMaxVelocityRPM;
+  LoggedTunableNumber shooterMaxAccelerationRPMPerSecond;
+
+  LoggedTunableNumber shooterTuningRPM;
+  LoggedTunableNumber shooterTuningAmps;
+  LoggedTunableNumber shooterTuningVolts;
+
+  // State variables
+  private final MutAngularVelocity targetVelocity = RPM.mutable(0.0);
+
+  public ShooterSubsystem(
+      DependencyOrderedExecutor dependencyOrderedExecutor,
+      MotorIO leadMotor,
+      MotorIO topFollowerMotor,
+      MotorIO bottomFollowerMotor) {
+    this.leadMotor = leadMotor;
+    this.topFollowerMotor = topFollowerMotor;
+    this.bottomFollowerMotor = bottomFollowerMotor;
+
+    stateMachine = new StateMachine<>(this);
+
+    velocityControlState = stateMachine.registerState(new VelocityControlState());
+    testModeState = stateMachine.registerState(new TestModeState());
+
+    velocityControlState
+        .when(shooter -> shooter.isShooterTestMode(), "Is shooter test mode")
+        .transitionTo(testModeState);
+
+    testModeState
+        .when(shooter -> !shooter.isShooterTestMode(), "Is not shooter test mode")
+        .transitionTo(velocityControlState);
+
+    stateMachine.setState(velocityControlState);
+
+    // Initialize tunable numbers for test modes
+    shooterKP =
+        new LoggedTunableNumber(
+            "ShooterTunables/shooterKP", JsonConstants.shooterConstants.shooterKP);
+    shooterKI =
+        new LoggedTunableNumber(
+            "ShooterTunables/shooterKI", JsonConstants.shooterConstants.shooterKI);
+    shooterKD =
+        new LoggedTunableNumber(
+            "ShooterTunables/shooterKD", JsonConstants.shooterConstants.shooterKD);
+
+    shooterKS =
+        new LoggedTunableNumber(
+            "ShooterTunables/shooterKS", JsonConstants.shooterConstants.shooterKS);
+    shooterKV =
+        new LoggedTunableNumber(
+            "ShooterTunables/shooterKV", JsonConstants.shooterConstants.shooterKV);
+    shooterKA =
+        new LoggedTunableNumber(
+            "ShooterTunables/shooterKA", JsonConstants.shooterConstants.shooterKA);
+
+    shooterMaxVelocityRPM =
+        new LoggedTunableNumber(
+            "ShooterTunables/shooterMaxVelocityRPM",
+            JsonConstants.shooterConstants.shooterMaxVelocity.in(RPM));
+    shooterMaxAccelerationRPMPerSecond =
+        new LoggedTunableNumber(
+            "ShooterTunables/shooterMaxAccelerationRPMPerSecond",
+            JsonConstants.shooterConstants.shooterMaxAcceleration.in(RPM.per(Second)));
+
+    shooterTuningRPM = new LoggedTunableNumber("ShooterTunables/shooterTuningRPM", 0.0);
+    shooterTuningAmps = new LoggedTunableNumber("ShooterTunables/shooterTuningAmps", 0.0);
+    shooterTuningVolts = new LoggedTunableNumber("ShooterTunables/shooterTuningVolts", 0.0);
+
+    dependencyOrderedExecutor.registerAction(UPDATE_INPUTS, this::updateInputs);
+  }
+
+  private void updateInputs() {
+    leadMotor.updateInputs(leadMotorInputs);
+    topFollowerMotor.updateInputs(topFollowerMotorInputs);
+    bottomFollowerMotor.updateInputs(bottomFollowerMotorInputs);
+
+    Logger.processInputs("Shooter/LeadMotorInputs", leadMotorInputs);
+    Logger.processInputs("Shooter/TopFollowerMotorInputs", topFollowerMotorInputs);
+    Logger.processInputs("Shooter/BottomFollowerInputs", bottomFollowerMotorInputs);
+
+    Logger.recordOutput(
+        "Shooter/ClosedLoopReferenceRotationsPerSecond", leadMotorInputs.closedLoopReference);
+    Logger.recordOutput(
+        "Shooter/ClosedLoopReferenceSlopeRotationsPerSecondPerSecond",
+        leadMotorInputs.closedLoopReferenceSlope);
+  }
+
+  @Override
+  public void monitoredPeriodic() {
+    Logger.recordOutput("Shooter/TargetVelocity", targetVelocity);
+
+    stateMachine.periodic();
+  }
+
+  protected void testPeriodic() {
+    switch (testModeManager.getTestMode()) {
+      case ShooterClosedLoopTuning -> {
+        LoggedTunableNumber.ifChanged(
+            hashCode(),
+            (pid_sva) -> {
+              leadMotor.setGains(
+                  pid_sva[0], pid_sva[1], pid_sva[2], pid_sva[3], 0, pid_sva[4], pid_sva[5]);
+            },
+            shooterKP,
+            shooterKI,
+            shooterKD,
+            shooterKS,
+            shooterKV,
+            shooterKA);
+
+        LoggedTunableNumber.ifChanged(
+            hashCode(),
+            (maxProfile) -> {
+              leadMotor.setProfileConstraints(
+                  MotionProfileConfig.immutable(
+                      RPM.of(maxProfile[0]),
+                      RPM.per(Second).of(maxProfile[1]),
+                      RotationsPerSecondPerSecond.zero().div(Seconds.of(1.0)),
+                      Volts.zero().div(RPM.of(1.0)),
+                      Volts.zero().div(RotationsPerSecondPerSecond.of(1.0))));
+            },
+            shooterMaxVelocityRPM,
+            shooterMaxAccelerationRPMPerSecond);
+
+        leadMotor.controlToVelocityProfiled(RPM.of(shooterTuningRPM.getAsDouble()));
+      }
+      case ShooterCurrentTuning -> {
+        leadMotor.controlOpenLoopCurrent(Amps.of(shooterTuningAmps.getAsDouble()));
+      }
+      case ShooterVoltageTuning -> {
+        leadMotor.controlOpenLoopVoltage(Volts.of(shooterTuningVolts.getAsDouble()));
+      }
+      default -> {}
+    }
+  }
+
+  protected void controlToTargetVelocity() {
+    leadMotor.controlToVelocityProfiled(targetVelocity);
+  }
+
+  public boolean isShooterTestMode() {
+    return switch (testModeManager.getTestMode()) {
+      case ShooterClosedLoopTuning,
+          ShooterCurrentTuning,
+          ShooterPhoenixTuning,
+          ShooterVoltageTuning ->
+          true;
+      default -> false;
+    };
+  }
+}
