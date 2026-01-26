59a8fe52711ece369ab81ae78f42b8fea81e82d6
aidnem <99768676+aidnem@users.noreply.github.com>
2026-01-26 13:59:51 -0500

    (#23) Implement shooter subsystem

--- src/main/java/frc/robot/subsystems/turret/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/turret/TurretSubsystem.java
@@ -95,7 +95,6 @@ public class TurretSubsystem extends MonitoredSubsystem {
   LoggedTunableNumber turretKS;
   LoggedTunableNumber turretKV;
   LoggedTunableNumber turretKA;
-  LoggedTunableNumber turretKG;
 
   LoggedTunableNumber turretExpoKV;
   LoggedTunableNumber turretExpoKA;
