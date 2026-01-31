f04467f7d4db3b8ec5fe09bd2cd5fa234f560d09
aidnem <99768676+aidnem@users.noreply.github.com>
2026-01-31 16:33:38 -0500

    (#22) Fix typo causing hopperConstants to be published at /hood route

--- src/main/java/frc/robot/constants/JsonConstants.java
+++ src/main/java/frc/robot/constants/JsonConstants.java
@@ -43,7 +43,7 @@ public class JsonConstants {
         jsonHandler.addRoute("/hopper", hopperConstants);
         jsonHandler.addRoute("/indexer", indexerConstants);
         jsonHandler.addRoute("/turret", turretConstants);
-        jsonHandler.addRoute("/hood", hopperConstants);
+        jsonHandler.addRoute("/hood", hoodConstants);
       } catch (Exception ex) {
         System.err.println("could not add routes for constant tuning: " + ex);
       }
