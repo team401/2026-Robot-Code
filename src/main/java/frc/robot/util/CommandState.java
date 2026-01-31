f884940bda4c1d3be3ec4a9e639b1e6081a2f56c
avidraccoon <nathansmith@mcps.org>
2026-01-31 17:00:14 -0500

    Enhance CommandState with detailed documentation and lifecycle methods for command management

--- src/main/java/frc/robot/util/CommandState.java
+++ src/main/java/frc/robot/util/CommandState.java
@@ -29,23 +29,62 @@ public class CommandState<World> extends State<World> {
     
     private Command command;
 
+    /**
+     * Creates a new CommandState with the given name and a null initial state.
+     *
+     * This constructor delegates to the two-argument constructor passing {@code null}
+     * for the state, indicating no explicit state is provided at construction time.
+     *
+     * @param name the name identifying this CommandState
+     */
     public CommandState(String name) {
         this(name, null);
     }
 
+    /**
+     * Constructs a new CommandState with the specified name and associated Command.
+     *
+     * The provided name is forwarded to the superclass constructor and is used to
+     * identify this state. The command parameter is stored and represents the
+     * Command associated with this state.
+     *
+     * @param name the human-readable name for this state; forwarded to the superclass
+     * @param command the Command to associate with this state
+     */
     public CommandState(String name, Command command) {
         super(name);
         this.command = command;
     }
 
+    /**
+     * Returns the Command associated with this CommandState.
+     *
+     * @return the current Command for this state, or null if no command is set
+     */
     public Command getCommand() {
         return command;
     }
 
+    /**
+     * Sets the current Command for this CommandState.
+     *
+     * @param command the Command to set as the current command; may be {@code null} to clear the current command
+     */
     public void setCommand(Command command) {
         this.command = command;
     }
 
+    /**
+     * Called when this state is entered by the state machine.
+     *
+     * <p>Invoked during a transition into this state. If a non-null {@code command} is
+     * associated with the state, its {@code initialize()} method is invoked to perform
+     * any setup required before the command runs. If {@code command} is null, no action
+     * is taken.</p>
+     *
+     * @param stateMachine the state machine that triggered the transition
+     * @param world the current world/context provided to the state
+     */
     @Override
     protected void onEntry(StateMachine<World> stateMachine, World world) {
         if (command != null) {
@@ -53,6 +92,18 @@ public class CommandState<World> extends State<World> {
         }
     }
 
+    /**
+     * Periodic update hook invoked by the owning state machine for this state.
+     * If this state has an associated command (i.e. the internal {@code command} reference is non-null),
+     * that command will be executed once per invocation of this method.
+     *
+     * Implementations should keep work performed here short and non-blocking to avoid delaying
+     * the state machine's periodic cycle.
+     *
+     * @param stateMachine the state machine that owns and invoked this state; may be used to request transitions
+     *                     or query global state
+     * @param world        the shared world/context object provided to states for reading or modifying
+     */
     @Override
     protected void periodic(StateMachine<World> stateMachine, World world) {
         if (command != null) {
@@ -60,6 +111,16 @@ public class CommandState<World> extends State<World> {
         }
     }
 
+    /**
+     * Called when this state is exited by the state machine.
+     *
+     * If there is an associated command, this will terminate that command by calling
+     * its end(false) method (indicating a normal completion rather than an interruption).
+     * A null-check is performed so there is no action when no command is present.
+     *
+     * @param stateMachine the StateMachine that is performing the transition away from this state
+     * @param world the shared World/context instance associated with the state machine
+     */
     @Override
     protected void onExit(StateMachine<World> stateMachine, World world) {
         if (command != null) {
