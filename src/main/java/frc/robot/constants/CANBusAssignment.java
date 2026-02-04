ff45f6d94fb1eb0e36ac15c6c9f2ef0cfd0747d2
avidraccoon <nathansmith@mcps.org>
2026-02-03 19:06:30 -0500

    Reorder CAN IDs in CANBusAssignment for better organization and clarity

--- src/main/java/frc/robot/constants/CANBusAssignment.java
+++ src/main/java/frc/robot/constants/CANBusAssignment.java
@@ -1,15 +1,6 @@
 package frc.robot.constants;
 
 public class CANBusAssignment {
-  public final Integer turretKrakenId = 9; // TODO: Verify this ID
-
-  public final Integer indexerKrakenId = 10; // TODO: Verify this ID
-
-  public final Integer hopperKrakenId = 11; // TODO: Verify this ID
-
-  public final Integer hoodKrakenId = 13; // TODO: Real ID
-
-  public final Integer homingSwitchCANdiID = 14; // TODO: Real id
 
   // Drivetrain CAN IDs
 
@@ -28,4 +19,18 @@ public class CANBusAssignment {
   public final Integer backRightDriveKrakenId = 3; // TODO: Verify this ID
   public final Integer backRightSteerKrakenId = 4; // TODO: Verify this ID
   public final Integer backRightEncoderId = 11; // TODO: Verify this ID
+
+  // The rest of the CAN IDs Should start from _GET_VALUE__ and go up from there
+
+  public final Integer turretKrakenId = 9; // TODO: Verify this ID
+
+  public final Integer indexerKrakenId = 10; // TODO: Verify this ID
+
+  public final Integer hopperKrakenId = 11; // TODO: Verify this ID
+
+  public final Integer hoodKrakenId = 13; // TODO: Real ID
+
+  public final Integer homingSwitchCANdiID = 14; // TODO: Real id
+
+  
 }
