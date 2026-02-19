6b071c80da81d483eb92e28af5c6738f5a9557dd
avidraccoon <nathansmith@mcps.org>
2026-02-19 10:41:43 -0500

    Refactor PID gains initialization in IntakeConstants to use kPID method for consistency

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -68,8 +68,8 @@ public class IntakeConstants {
   public final Voltage homingVoltage = Volts.of(-2.0);
 
   // PID GAINS
-  public PIDGains pivotPIDGains = new PIDGains(0.5, 0.0, 0.1); // These values are placeholders
-  public PIDGains rollersPIDGains = new PIDGains(20.0, 10.0, 10.0); // These values are placeholders
+  public PIDGains pivotPIDGains = PIDGains.kPID(0.5, 0.0, 0.1); // These values are placeholders
+  public PIDGains rollersPIDGains = PIDGains.kPID(20.0, 10.0, 10.0); // These values are placeholders
 
   // Current Limits (These current limits are placeholders and were picked randomly)
   public final Current pivotSupplyCurrentLimit = Amps.of(40.0);
