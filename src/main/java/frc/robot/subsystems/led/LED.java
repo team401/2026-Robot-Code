81acf60a39e3fe84ea2516b3915ca342dd27fb00
Josh <joshua.azro@gmail.com>
2026-02-28 13:13:36 -0500

    working on leds

--- src/main/java/frc/robot/subsystems/led/LED.java
+++ src/main/java/frc/robot/subsystems/led/LED.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems;
+package frc.robot.subsystems.led;
 
 import static edu.wpi.first.units.Units.*;
 
@@ -18,6 +18,7 @@ import frc.robot.subsystems.hopper.HopperSubsystem;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 import frc.robot.subsystems.turret.TurretSubsystem;
 import java.util.Optional;
+import org.littletonrobotics.junction.networktables.LoggedNetworkBoolean;
 
 public class LED extends SubsystemBase {
   private Optional<Drive> driveSubsystem;
@@ -30,11 +31,13 @@ public class LED extends SubsystemBase {
   public static ConnectorXAnimate led = new ConnectorXAnimate();
   private boolean ledConnected = led.Connect(USBPort.kUSB1);
   private boolean directLedEnabled = false;
-  // Track whether we were disabled in the previous periodic call so we only send the
-  // animation command once when entering the disabled state (prevents spamming the device
-  // with the same animation every loop).
+
   private boolean wasDisabled = false;
+  private boolean disabled = DriverStation.isDisabled();
+
+  private boolean wasHomed = false;
   private boolean isHomed = false;
+  private final LoggedNetworkBoolean isHomedSpoof = new LoggedNetworkBoolean("LED/isHomed");
 
   /* Lumyn Device Config creates a config build for everything that
    * LEDs are going to do (ie. zones, channels, animations)
@@ -43,11 +46,30 @@ public class LED extends SubsystemBase {
     return new ConfigBuilder()
         .forTeam("401")
         // Channels
-        // TODO: these are all made up because design isn't done designing, will update
-        .addChannel(1, "1", 160)
-        .addStripZone("1", 160, false)
+
+        .addChannel(1, "backStrip", 94)
+        .addStripZone("leftBack", 47, false)
+        .addStripZone("rightBack", 47, false)
+        .endChannel()
+        .addChannel(2, "frontStrip", 102)
+        .addStripZone("leftFront", 51, false)
+        .addStripZone("rightFront", 51, false)
         .endChannel()
         // Group
+        .addGroup("backStrip")
+        .addZone("leftBack")
+        .addZone("rightBack")
+        .endGroup()
+        .addGroup("frontStrip")
+        .addZone("leftFront")
+        .addZone("rightFront")
+        .endGroup()
+        .addGroup("all")
+        .addZone("leftBack")
+        .addZone("rightBack")
+        .addZone("leftFront")
+        .addZone("rightFront")
+        .endGroup()
         .build();
   }
 
@@ -67,45 +89,47 @@ public class LED extends SubsystemBase {
 
     led.Connect(USBPort.kUSB1);
     led.ApplyConfiguration(buildConfig());
-    led.leds.SetAnimation(Animation.Fill)
-        .ForGroup("1")
-        .WithColor(Color.kYellow)
-        .RunOnce(true); // see time between init and periodic
+    clearGroup("all");
+    setGroupColor("all", Color.kYellow); // see time between init and periodic
 
     SmartDashboard.putBoolean("led/isHomed", isHomed);
   }
 
   public void periodic() {
     isHomed = SmartDashboard.getBoolean("led/isHomed", false);
-    if (isHomed == false) {
-      led.leds.SetAnimation(Animation.Fill).ForZone("1").WithColor(Color.kMagenta).RunOnce(true);
+    // check if homed switch has been triggered
+    if (!isHomed) {
+      setGroupColor("all", Color.kOrangeRed);
+      wasHomed = false;
       return;
-    }
-    boolean disabled = DriverStation.isDisabled();
-
-    // When we first enter the disabled state, start the looping rainbow animation once. The
-    // animation itself will run continuously on the LED controller, so we don't need to keep
-    // re-sending the command every periodic call (which can spam the device).
-    if (disabled && !wasDisabled) {
-      led.leds.SetAnimation(Animation.RainbowCycle)
-          .ForZone("1")
-          .WithDelay(Milliseconds.of(10))
-          .RunOnce(false);
+    } else if (isHomed && !wasHomed) {
+      if (disabled && !wasDisabled) {
+        led.leds.SetAnimation(Animation.RainbowRoll)
+            .ForGroup("all")
+            .WithDelay(Milliseconds.of(6))
+            .RunOnce(false);
+      }
     }
 
-    // When we transition back to enabled, set a single color update (only once on transition)
-    // to avoid spamming the controller every loop.
     if (!disabled && wasDisabled) {
-      led.leds.SetAnimation(Animation.Fill).ForZone("1").WithColor(Color.kBlack).RunOnce(true);
-      // led.leds.SetGroupColor("1", Color.kPurple);
-      led.leds.SetColor("1", Color.kPink);
+      led.leds.SetAnimation(Animation.Fill).ForGroup("all").WithColor(Color.kBlack).RunOnce(true);
+
+      led.leds.SetColor("rightFront", Color.kPink);
     }
 
-    // Update state for next periodic call
     wasDisabled = disabled;
+    wasHomed = isHomed;
   }
 
   public void setHomed(boolean homed) {
     isHomed = homed;
+  }
+
+  public void clearGroup(String group) {
+    led.leds.SetAnimation(Animation.Fill).WithColor(Color.kBlack).ForGroup(group).RunOnce(true);
+  }
+
+  public void setGroupColor(String group, Color color) {
+    led.leds.SetAnimation(Animation.Fill).WithColor(color).ForGroup(group).RunOnce(true);
   }
 }
